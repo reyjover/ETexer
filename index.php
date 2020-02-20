e8c5394d5b01bfdee5b1d4430539c4e1f771ec4e
reyjover <60122552+reyjover@users.noreply.github.com>
2020-02-20 11:22:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,19 @@ $loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav'
     </head>
     <body>
                         <ul> 
-                        <li> <a href="index.php?loadnav=read"> Show </a> </li>
+                        <li> <a href="index.php?loadnav=readprod"> Show </a> </li>
+                        <li> <a href="index.php?loadnav=readcat"> Show Categories </a> </li>
                         <li> <a href="index.php?loadnav=create"> Create  </a> </li>
                         <li> <a href="index.php?loadnav=delete"> Delete  </a> </li>
                         </ul>
                
                         <?php 
                         switch($loadnav){
-                            case 'read':
+                            case 'readprod':
                             require_once 'show.php';
+                            break;
+                            case 'readcat':
+                            require_once 'show_categories.php';
                             break;
                             case 'create':
                             require_once 'form_create.php';
