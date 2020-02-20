d15bbb8b514b9db802fcb7c17a37b2768e617a15
reyjover <60122552+reyjover@users.noreply.github.com>
2020-02-20 11:12:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
+<?php 
+$loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav']: '';
+?>
+<html> 
+    <head>
+      <title>  Endterm Activity </title>
+    </head>
+    <body>
+                        <ul> 
+                        <li> <a href="index.php?loadnav=read"> Show </a> </li>
+                        <li> <a href="index.php?loadnav=create"> Create  </a> </li>
+                        <li> <a href="index.php?loadnav=delete"> Delete  </a> </li>
+                        </ul>
+               
+                        <?php 
+                        switch($loadnav){
+                            case 'read':
+                            require_once 'show.php';
+                            break;
+                            case 'create':
+                            require_once 'form_create.php';
+                            break;
+                            case 'delete':
+                            require_once 'form_delete.php';
+                            break;
+                        }
+                        ?>
 
+    </body>
+</html>
