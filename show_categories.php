16232d31a2f581f45a71d0630fc82bffc0721952
reyjover <60122552+reyjover@users.noreply.github.com>
2020-02-20 11:23:13 +0800

    Update show_categories.php

--- show_categories.php
+++ show_categories.php
@@ -1,1 +1,28 @@
+<?php
+$json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
+
+$data = json_decode($json,true);
+$list = $data['records'];
+
+
+?>
+
+<h1> Categories </h1>
+
+<table border="1px">
+    <tr>
+        <td>Category ID</td>
+        <td>Category Name</td>
+    </tr>
+<?php
+foreach($list as $value){
+    ?>
+    <tr>
+        <td><?php echo $value['category_id'];?></td>
+        <td><?php echo $value['category_name'];?></td>
+    </tr>
+<?php
+}
+?>
+</table>
 
