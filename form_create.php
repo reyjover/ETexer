8379b934af98fd9c3bf71b1e13f2c8372ca0631c
reyjover <60122552+reyjover@users.noreply.github.com>
2020-02-26 21:31:27 +0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -1,8 +1,24 @@
+<?php
+	//category
+	$jsonCat = file_get_contents('http://rdapi.herokuapp.com/category/read.php');
+	$catData = json_decode($jsonCat,true);
+	$category = $catData['records'];
+?>
+<html>
 <form action="pro_create.php" method="POST">
-<input type="text" name="name" placeholder="name"/>
-<input type="text" name="description" placeholder="description"/>
-<input type="text" name="price" placeholder="price"/>
-<input type="text" name="category" placeholder="category"/>
-<input type="submit" name="submit" value="submit"/>
-
+	Name:<input type="text" name="name" placeholder="Enter Product Name"/><br/><br/>
+	Description:<textarea name="description" placeholder="Enter Item Description"/></textarea><br/><br/>
+	Price: <input type="number" name="price" placeholder="Enter Product Price"/><br/><br/>
+	Category:<selectname="category">
+		<option value="">--Select Category--</option>
+	<?php
+      foreach($category as $cview){
+    ?>
+		<option value="<?php echo $cview['id']?>"><?php echo $cview['name']?></option>
+    <?php
+      }
+    ?>
+	</select>
+	<input type="submit" name="submit" value="Create"/>
 </form>
+</html>
