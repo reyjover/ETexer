2cc7e1da8fae8d95b1f65593d1507448cf380273
Thanny <60122552+reyjover@users.noreply.github.com>
2020-03-03 10:50:10 +0800

    Update show.php

--- show.php
+++ show.php
@@ -4,11 +4,22 @@ $json = file_get_contents("http://rdapi.herokuapp.com/product/read.php");
 $data = json_decode($json,true);
 $list = $data['records'];
 
+if(isset($search)){
+	$jsearch = file_get_contents('http://rdapi.herokuapp.com/product/search.php?s='.$search);
+	$res = json_decode($jsearch,true);
 
+	$list = $res['records'];
+	
+ }else{
+	$list = $data['records'];
+ }
 ?>
 
 <h1 class="header"> Products Overview </h1>
-
+<form action="index.php?page=readprod" method="POST">
+	Search: <input type="text" name="search" placeholder="Search Product Name">
+		<input type="submit" name="submit" value="Search">
+	</form>
 <table>
     <tr>
         <td class="label">Product</td>
