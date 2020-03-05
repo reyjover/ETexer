5300c6bb2b91d7831d6ae5ddca3fafbcb1902ed9
Thanny <60122552+reyjover@users.noreply.github.com>
2020-03-05 10:51:21 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -15,7 +15,7 @@
       <link href="https://fonts.googleapis.com/css?family=Kanit:800i|Oswald|Teko:600&display=swap" rel="stylesheet">
 <br/>
 <div class="wrapper">
-<form action="pro_update.php?id=<?php echo $id ?>" method="POST">
+<form action="index.php?loadnav=pro_update&&id=<?php echo $id ?>" method="POST">
 	<div class="formlabels"> Name: </div> <input type="text" name="name" value="<?php echo $result['name'];?>"/><br/>
 	<div class="formlabels">Description:</div> <textarea name="description"><?php echo $result['description']; ?></textarea><br/><br/>
 	<div class="formlabels">Price:</div> <input type="number" name="price" value="<?php echo $result['price']; ?>"/><br/><br/>
