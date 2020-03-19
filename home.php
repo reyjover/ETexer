7ce87c4942fc624fa80e0734269e3b9888650d45
Thanny <60122552+reyjover@users.noreply.github.com>
2020-03-19 20:34:18 +0800

    Update home.php

--- home.php
+++ home.php
@@ -20,7 +20,7 @@ if(isset($search)){
 ?>
 <html>
 <head> 
-<title> Endterm Activity </title> 
+<title> Endterm | Final Exam </title> 
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <link href="https://fonts.googleapis.com/css?family=Kanit:800i|Oswald|Teko:600&display=swap" rel="stylesheet">
 </head>
